b9e05d7c3f6b20c0a9a24977d39c41727cf8f8da
umar83 <umarhatob@gmail.com>
2017-10-21 22:29:41 +0700

    Create inew.php

---
+++ inew.php
@@ -0,0 +1,19 @@
+<script type="text/javascript" src="https://cdn.rawgit.com/umar83/jwplayer/f13c05d7/jwplayer-7.9.3/jwplayermetro.js"></script>
+<script>jwplayer.key="ypdL3Acgwp4Uh2/LDE9dYh3W/EPwDMuA2yid4ytssfI=";</script>
+<div id="myElement"></div>
+<script type="text/javascript">                    
+jwplayer('myElement').setup({
+playlist:[{file:'https://cdn-livetv.metube.id/hls/rcti.m3u8',
+title:'RCTI',mediaid:'5'}],
+width:"100%",aspectratio:"16:9",
+abouttext:'tv3ku.blogspot.com',
+stretching:"uniform",
+aboutlink:'',
+autostart:false,
+androidhls:true,
+controls:true,
+displaydescription:true,
+displaytitle:true,
+repeat:true,
+stagevideo:false,
+visualplaylist:false,}); </script>
